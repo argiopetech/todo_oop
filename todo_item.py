1761823328faaad4bf1563346c4df08a911123f4
Elliot Robinson <elliot.robinson@argiopetech.com>
2022-11-15 10:29:48 -0500

    Limbo-ing! See tags/releases for what you'd normally expect in a commit message.

--- todo_item.py
+++ todo_item.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
 class TodoItem:
     def __init__(self, description):
         self.description = description
         self.complete = False
+
+        self.creationDate = datetime.now()
