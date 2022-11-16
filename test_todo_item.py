4e554fae36c60199d2a0d9e0d904307a4428f3df
Elliot Robinson <elliot.robinson@argiopetech.com>
2022-11-16 08:19:03 -0500

    Final(?) version

--- test_todo_item.py
+++ test_todo_item.py
@@ -1,49 +1,50 @@
 from todo_item import *
 
-def test_create_TodoItem(taskDescription = "Mow lawn"):
-  item = TodoItem(taskDescription)
+def test_create_TodoItem():
+  def doTest(taskDescription):
+    item = TodoItem(taskDescription)
 
-  assert(item.description == taskDescription)
-  assert(item.complete == False)
-  assert(item.creationDate)
+    assert(item.description == taskDescription)
+    assert(item.complete == False)
+    assert(item.creationDate)
 
-
-def test_save_TodoItem(taskDescription = "Mow lawn"):
-  item = TodoItem(taskDescription)
-
-  result = item.save()
-  assert(result == f"False|{taskDescription}")
-
-  item.markComplete()
-  result = item.save()
-  assert(result == f"True|{taskDescription}")
+  doTest("Mow lawn")
+  doTest("Test 1")
+  doTest("Test 2")
+  doTest("Test 3")
 
 
-def test_other_tasks():
-  taskList = ["Task 1", "Task 2", "Task 3"]
+def test_save_TodoItem():
+  def doTest(taskDescription):
+    item = TodoItem(taskDescription)
 
-  for task in taskList:
-    test_create_TodoItem(task)
-    test_save_TodoItem(task)
+    result = item.save()
+    assert(result == f"False|{taskDescription}")
+
+    item.markComplete()
+    result = item.save()
+    assert(result == f"True|{taskDescription}")
+
+  doTest("Mow lawn")
+  doTest("Test 1")
+  doTest("Test 2")
+  doTest("Test 3")
 
 
-def test_task_completion(taskDescription = "Mow lawn"):
-  item = TodoItem(taskDescription)
+def test_task_completion():
+  def doTest(taskDescription):
+    item = TodoItem(taskDescription)
 
-  assert(item.complete == False)
+    assert(item.complete == False)
 
-  item.markComplete()
+    item.markComplete()
 
-  assert(item.complete == True)
+    assert(item.complete == True)
 
-
-def test_empty_todo():
-  try:
-    TodoItem("")
-  except ValueError:
-    return
-
-  assert(False)
+  doTest("Mow lawn")
+  doTest("Test 1")
+  doTest("Test 2")
+  doTest("Test 3")
 
 
 def test_load_todo():
@@ -62,3 +63,12 @@ def test_load_todo():
 
   doTest("Mow lawn")
   doTest("Mow|Lawn")
+
+
+def test_empty_todo():
+  try:
+    TodoItem("")
+  except ValueError:
+    return
+
+  assert(False)
